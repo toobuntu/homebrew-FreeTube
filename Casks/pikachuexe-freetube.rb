dd3f7eddfafe370798b8189411fbde2284227da8
Todd Schulman <toobuntu@gmail.com>
2024-03-26 19:17:12 -0400

    test-audit

--- Casks/pikachuexe-freetube.rb
+++ Casks/pikachuexe-freetube.rb
@@ -1,6 +1,6 @@
 cask "pikachuexe-freetube" do
-  version "0.19.1"
-  sha256 "0ff50bd11a372166b7860c7eedfe8757b1411b71b4356ea40849fee8c6b7ccdb"
+  version "0.19.2"
+  sha256 "315b3ca898bf15681505222a80cab19888d402b8748506fa663f9d525db8c229"
 
   url "https://github.com/PikachuEXE/homebrew-FreeTube/releases/download/v#{version}-beta/freetube-#{version}-mac-arm64.dmg"
   name "FreeTube"
